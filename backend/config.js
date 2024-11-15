c16dfc326377da68e6bf01fca840016b86e6bdad
rkddbwns0 <112998995+rkddbwns0@users.noreply.github.com>
2024-11-15 22:22:32 +0900

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-const SPOTIFY_CLIENT_ID = '53af9f0897374cb9bba9ffa18848b838';
-const SPOFIFY_CLENT_SECRET = '83c75f2f687343e895b2fccaf2ab04cf';
-const SCOPE =
-    'ugc-image-upload%20user-read-playback-state%20user-read-currently-playing%20playlist-read-private%20streaming%20playlist-read-collaborative%20user-read-recently-played';
-const REDIRECT_URI = 'http://localhost:8080/callback';
-
-module.exports = { SPOTIFY_CLIENT_ID, SPOFIFY_CLENT_SECRET, SCOPE, REDIRECT_URI };
